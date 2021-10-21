33b496a04b792b21e9347a2cd8445a91a2804838
Roberto Sousa <robertosousa186@gmail.com>
2021-10-21 00:26:11 -0300

    Test passing

--- src/service/user.ts
+++ src/service/user.ts
@@ -5,6 +5,7 @@ const users: Array<IUsers> = [];
 
 const create = (user: IUsers) => {
   if (!user.email) throw new Error('Por favor, informe o campo email.');
+  if (!user.password) throw new Error('Por favor, informe o campo senha.');
 
   users.push(user);
 
