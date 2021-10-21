4d488a4934165450405d433b353b3f3046771c7f
Roberto Sousa <robertosousa186@gmail.com>
2021-10-20 23:48:39 -0300

    Creating test structure

--- __tests__/user.spec.ts
+++ __tests__/user.spec.ts
@@ -1,4 +1,5 @@
-import { createUser } from '../src/service/userService';
+import server from '../src/server';
+import request from 'supertest';
 import { IUsers } from '../src/types/IUsers';
 
 const validUser = () => {
@@ -10,8 +11,16 @@ const validUser = () => {
   return user;
 };
 
+console.log(validUser);
 describe('userService', () => {
-  it('should be return 200 when a valid user is create', () => {
-    
+  it('should be return 200 when a valid user is create', (done) => {
+    const user = validUser();
+    request(server)
+      .post('/create')
+      .send(user)
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
   });
 });
