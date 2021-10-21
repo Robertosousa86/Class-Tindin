524d32d113dd49ec9908a6753e80700a866ce17b
Roberto Sousa <robertosousa186@gmail.com>
2021-10-21 00:12:28 -0300

    Creating error message

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,8 +1,11 @@
+import { response } from 'express';
 import { IUsers } from '../types/IUsers';
 
 const users: Array<IUsers> = [];
 
 const create = (user: IUsers) => {
+  if (!user.email) throw new Error('Por favor, informe o campo email.');
+
   users.push(user);
 
   return user;
