825625607329d5e27228b4da312538c7e74200e6
Roberto Sousa <robertosousa186@gmail.com>
2021-10-20 23:54:10 -0300

    Implementing route for post method in create

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { json } from 'express';
+import * as user from '../src/controller/user';
 
 const server = express();
+
+server.use(json());
 
 server.get('/', (req, res) => {
   return res.send('Hello Tindin =)');
 });
 
-export { server };
+server.post('/create', user.create);
+
+export default server;
