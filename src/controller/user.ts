f2d76a5c28b2b2c8aa7468fe0cb789cba9a25709
Roberto Sousa <robertosousa186@gmail.com>
2021-10-21 00:19:01 -0300

    Implementing try/catch to error handling

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express';
 import * as user from '../service/user';
+import { badRequest } from '../libs/bindError';
 
 const create = (req: Request<any>, res: Response<any>) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  return res.status(200).json(user.create({ email, password }));
+    return res.status(200).json(user.create({ email, password }));
+  } catch (error) {
+    return badRequest(res, error);
+  }
 };
 
 export { create };
