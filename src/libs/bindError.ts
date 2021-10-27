6acd0d39ab8f97d9f44d5c8a3eadca5827cca4f5
Roberto Sousa <robertosousa186@gmail.com>
2021-10-27 00:55:11 -0300

    Creating function notFound

--- src/libs/bindError.ts
+++ src/libs/bindError.ts
@@ -4,4 +4,8 @@ const badRequest = (res: Response<any>, error: any) => {
   return res.status(400).json({ message: error.message });
 };
 
-export { badRequest };
+const notFound = (res: Response<any>, error: any) => {
+  return res.status(404).json({ message: error.message });
+};
+
+export { badRequest, notFound };
