77962af2533da50a80431bb7dc5bd659456836ee
Roberto Sousa <robertosousa186@gmail.com>
2021-10-27 20:06:55 -0300

    Creating routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express, { json } from 'express';
 import * as user from '../src/controller/user';
+import cors from 'cors';
 
 const server = express();
 
 server.use(json());
+server.use(cors());
 
 server.get('/', (req, res) => {
   return res.send('Hello Tindin =)');
 });
 
 server.post('/create', user.create);
+server.get('/users', user.list);
+server.get('/users/:id', user.get);
 
 export default server;
