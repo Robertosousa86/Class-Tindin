7f6a9b908e4cb9213a7a9f2588d02869332185a8
Roberto Sousa <robertosousa186@gmail.com>
2021-10-27 00:53:32 -0300

    Creating list and get

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,17 +1,39 @@
 import { Request, Response } from 'express';
 import * as user from '../service/user';
-import { badRequest } from '../libs/bindError';
+import { badRequest, notFound } from '../libs/bindError';
+
+const list = async (req: Request<any>, res: Response<any>) => {
+  try {
+    const users = await user.list();
+    
+    return res.json(users);
+  } catch (err) {
+    return badRequest(res, err);
+  }
+};
+
+const get = async (req: Request<any>, res: Response<any>) => {
+  try {
+    const { id } = req.params;
+
+    const foundUser = await user.get(id);
+
+    return res.json(foundUser);
+  } catch (err: any) {
+    return notFound(res, err);
+  }
+};
 
 const create = async (req: Request<any>, res: Response<any>) => {
   try {
     const { email, password } = req.body;
 
-    const userCreated = await user.create({ email, password });
+    await user.create({ email, password });
 
-    return res.status(200).json(userCreated);
+    return res.status(201).json({ message: 'Usuário criado com sucesso!' });
   } catch (error) {
     return badRequest(res, error);
   }
 };
 
-export { create };
+export { create, list, get };
