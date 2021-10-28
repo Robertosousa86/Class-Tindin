f1ce0047ac3e6a064fea636971f6429b0c25c1ec
Roberto Sousa <robertosousa186@gmail.com>
2021-10-27 23:53:12 -0300

    Creating update and remove

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -2,10 +2,24 @@ import { Request, Response } from 'express';
 import * as user from '../service/user';
 import { badRequest, notFound } from '../libs/bindError';
 
+const create = async (req: Request<any>, res: Response<any>) => {
+  try {
+    const { email, password } = req.body;
+
+    await user.create({ email, password });
+
+    return res
+      .status(201)
+      .json({ message: 'Usuário(a) criado(a) com sucesso!' });
+  } catch (error) {
+    return badRequest(res, error);
+  }
+};
+
 const list = async (req: Request<any>, res: Response<any>) => {
   try {
     const users = await user.list();
-    
+
     return res.json(users);
   } catch (err) {
     return badRequest(res, err);
@@ -24,16 +38,34 @@ const get = async (req: Request<any>, res: Response<any>) => {
   }
 };
 
-const create = async (req: Request<any>, res: Response<any>) => {
+const update = async (req: Request<any>, res: Response<any>) => {
   try {
-    const { email, password } = req.body;
+    const { id, email, password } = req.body;
 
-    await user.create({ email, password });
+    if (!id) return res.status(400).json({ message: 'Informe o campo id!' });
 
-    return res.status(201).json({ message: 'Usuário criado com sucesso!' });
-  } catch (error) {
-    return badRequest(res, error);
+    await user.update({ id, email, password });
+
+    return res
+      .status(200)
+      .json({ message: 'Dado(s) alterado(s) com sucesso!' });
+  } catch (err: any) {
+    return notFound(res, err);
   }
 };
 
-export { create, list, get };
+const remove = async (req: Request<any>, res: Response<any>) => {
+  try {
+    const id = req.body.id;
+
+    if (!id) return res.status(400).json({ message: 'Informe o campo id!' });
+
+    await user.remove(id);
+
+    res.status(200).json({ success: 'Usuário(a) deletado(a) com sucesso!' });
+  } catch (err: any) {
+    return notFound(res, err);
+  }
+};
+
+export { create, list, get, update, remove };
